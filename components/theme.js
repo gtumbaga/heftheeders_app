fd7c324cbe221c000e416f549074791e49ac574f
Gabriel Tumbaga <gabrieltumbaga@odachi.local>
2024-02-12 01:52:39 -0800

    added bootstrap 5, and added some config that does static builds

---
+++ components/theme.js
@@ -0,0 +1,11 @@
+export default function Theme({ children }) {
+    return (
+        <>
+            {/* <Navbar /> */}
+            <div>nav goes here</div>
+            <main>{children}</main>
+            <div>footer goes here</div>
+            {/* <Footer /> */}
+        </>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Theme from "@/components/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme>
+        <Component {...pageProps} />
+    </Theme>
+  );
 }
