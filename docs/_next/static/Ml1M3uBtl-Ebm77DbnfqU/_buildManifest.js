71c6c4553740a96bcdb53dd8082683eb3a37e5dc
Gabriel Tumbaga <gabrieltumbaga@odachi.local>
2024-02-12 19:38:45 -0800

    deploy files

---
+++ docs/_next/static/Ml1M3uBtl-Ebm77DbnfqU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/271-91e2015bfa0277fe.js","static/css/21f220772065b4a0.css","static/chunks/pages/index-54d5d4c60af0d64a.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
