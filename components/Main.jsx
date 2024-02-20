5f32a15e5e1341892ec0a1640443437c30381c32
gabe <djgtraxx@gmail.com>
2024-02-20 04:58:16 +0000

    got the weekly data being grabbed and sorted, now just need to put it into visuals

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,12 +9,16 @@ import LoadingWidget from "./common/LoadingWidget";
 const Main = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [authToken, setAuthToken] = useState(null);
+    const [dateStrings, setDateStrings] = useState(null);
+    const [weekData, setWeekData] = useState({});
 
     useEffect(() => {
         // check if we have a token stored in LS
         const tmpToken = localStorage.getItem('token');
 
         if (tmpToken) {
+            setAuthToken(tmpToken);
             setIsLoading(true);
             const url = `${process.env.NEXT_PUBLIC_API_URL}/api/user`
 
@@ -35,6 +39,95 @@ const Main = () => {
         }
     }, []);
 
+    useEffect(() => {
+        if (isLoggedIn === true) {
+            // user is logged in, so lets get the initial week
+            setIsLoading(true);
+            let currentDate = new Date();
+            // currentDate = new Date(new Date().setDate(new Date().getDate() + 4)); //this is the 23rd
+
+            // derive 1st day of current week (monday)
+            // 1: mon, 2: tue, 3: wed, 4: thu, 5: fri, 6: sat, 0: sun
+            const day = currentDate.getDay();
+            let dayAdjustment = 0;
+            if (day < 1) {
+                dayAdjustment = -6;
+            } else {
+                dayAdjustment = (day - 1) * -1;
+            }
+            const startDate = new Date(new Date().setDate(currentDate.getDate() + dayAdjustment));
+
+            const day2_Date = new Date(new Date().setDate(startDate.getDate() + 1));
+            const day3_Date = new Date(new Date().setDate(startDate.getDate() + 2));
+            const day4_Date = new Date(new Date().setDate(startDate.getDate() + 3));
+            const day5_Date = new Date(new Date().setDate(startDate.getDate() + 4));
+            const day6_Date = new Date(new Date().setDate(startDate.getDate() + 5));
+            const day7_Date = new Date(new Date().setDate(startDate.getDate() + 6));
+
+            const dateLabels = [
+                `${startDate.getFullYear()}-${startDate.getMonth()+1}-${startDate.getDate()}`,
+                `${day2_Date.getFullYear()}-${day2_Date.getMonth()+1}-${day2_Date.getDate()}`,
+                `${day3_Date.getFullYear()}-${day3_Date.getMonth()+1}-${day3_Date.getDate()}`,
+                `${day4_Date.getFullYear()}-${day4_Date.getMonth()+1}-${day4_Date.getDate()}`,
+                `${day5_Date.getFullYear()}-${day5_Date.getMonth()+1}-${day5_Date.getDate()}`,
+                `${day6_Date.getFullYear()}-${day6_Date.getMonth()+1}-${day6_Date.getDate()}`,
+                `${day7_Date.getFullYear()}-${day7_Date.getMonth()+1}-${day7_Date.getDate()}`,
+            ];
+
+            setDateStrings([...dateLabels]);
+
+            let tmpWeekData = {};
+
+            dateLabels.forEach((lab) => {
+                tmpWeekData[lab] = {
+                    items_breakfast: [],
+                    items_lunch: [],
+                    items_dinner: [],
+                    items_snacks: [],
+                    items_activity: [],
+                }
+            });
+
+            console.log('date data:', dateLabels);
+            console.log('day of week:', day);
+            console.log('dayadjust', dayAdjustment);
+
+            const url = `${process.env.NEXT_PUBLIC_API_URL}/api/week/${dateLabels[0]}`
+
+            const config = {
+                headers: { Authorization: `Bearer ${authToken}` }
+            };
+
+            axios.get(url, config)
+            .then((response) => {
+                if (response && response.data.success == true)
+                console.log('got week data', response.data);
+
+                //assemble full week data
+                response.data.days.forEach((dayData) => {
+                    console.log('iteration of dayData', dayData);
+                    tmpWeekData[dayData.this_day] = {
+                        items_breakfast: JSON.parse(dayData.items_breakfast),
+                        items_lunch: JSON.parse(dayData.items_lunch),
+                        items_dinner: JSON.parse(dayData.items_dinner),
+                        items_snacks: JSON.parse(dayData.items_snacks),
+                        items_activity: JSON.parse(dayData.items_activity),
+                    };
+                });
+
+                console.log('tmpWeekData', tmpWeekData);
+
+                setWeekData({...tmpWeekData});
+                setIsLoading(false);
+            });
+        }
+    }, [isLoggedIn]);
+
+    useEffect(() => {
+        if (weekData) {
+            console.log('week data has beenset', weekData);
+        }
+    }, [weekData]);
 
     const handleSubmit = useCallback(async (userEmail, userPW) => {
         const url = `${process.env.NEXT_PUBLIC_API_URL}/api/login`
@@ -58,7 +151,6 @@ const Main = () => {
             console.log('response is:', response);
         }
     });
-
 
     return(
         <>
